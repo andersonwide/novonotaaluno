82b7309f3fe34735f3c9a283048472b17999a100
andersonwide <michaelmags42@outlook.com>
2021-09-05 23:50:37 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Notaaluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] notaAluno = new double[2];
+            double res;
+
+            notaAluno[0] = double.Parse(Console.ReadLine());
+            while (notaAluno[0] < 0 || notaAluno[0] > 10)
+            {
+                Console.WriteLine("nota invalida");
+                notaAluno[0] = double.Parse(Console.ReadLine());
+            }
+            notaAluno[1] = double.Parse(Console.ReadLine());
+            while (notaAluno[1] < 0 || notaAluno[1] > 10)
+            {
+                Console.WriteLine("nota invalida");
+                notaAluno[1] = double.Parse(Console.ReadLine());
+            }
+            res = (notaAluno[0] + notaAluno[1]) / 2;
+            Console.WriteLine("media = " + res.ToString("N2"));
+        }
+    }
+}
